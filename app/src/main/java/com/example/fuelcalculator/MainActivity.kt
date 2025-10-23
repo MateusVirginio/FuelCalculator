839c2c34c81c3b0cec4f974c7b686697af276fc7
MateusVirginio <virginio.dev@gmail.com>
2025-10-22 23:30:05 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.fuelcalculator
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val edtDistancia = findViewById<EditText>(R.id.edtDistancia)
+        val edtKmLitro = findViewById<EditText>(R.id.edtKmLitro)
+        val edtPrecoLitro = findViewById<EditText>(R.id.edtPrecoLitro)
+
+        val txtResultado = findViewById<TextView>(R.id.txtResultado)
+
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+
+        btnCalcular.setOnClickListener {
+            val distancia = edtDistancia.text.toString().toInt()
+            val kmLitro = edtKmLitro.text.toString().toInt()
+            val precoLitro = edtPrecoLitro.text.toString().toDouble()
+
+            val litrosNecessarios = distancia / kmLitro
+
+            val custoTotal = litrosNecessarios * precoLitro
+
+            //MOSTRANDO RESULTADO FINAL NO TEXTVIEW
+            txtResultado.text = "Custo Total = $custoTotal"
+
+            println("Distância: $distancia")
+            println("Consumo: $kmLitro")
+            println("Preço por Litro: $precoLitro")
+
+            println("Custo Total: $custoTotal")
+        }
+    }
+}
