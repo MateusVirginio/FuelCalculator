ecb040ef6bf46a42d33a74c658439d9a9c693a02
MateusVirginio <virginio.dev@gmail.com>
2025-10-23 21:02:13 -0300

    Add labels and navigation for input screens
    Introduces descriptive TextViews to the distance, fuel price, and km/l screens for improved clarity. Updates input field hints and constraints for better UI alignment. Implements navigation from DistanciaActivity to KmLitroActivity, passing the entered distance value.

--- app/src/main/java/com/example/fuelcalculator/DistanciaActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/DistanciaActivity.kt
@@ -1,6 +1,9 @@
 package com.example.fuelcalculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,19 @@ class DistanciaActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //FindViewById
+        val edtDistancia = findViewById<EditText>(R.id.edtDistancia)
+        val btnDistancaProximo = findViewById<Button>(R.id.btnDistancia)
+
+        //NavegarProximaTela
+
+        btnDistancaProximo.setOnClickListener {
+            val edtDistanciaValor = edtDistancia.text.toString().toInt()
+            val intent = Intent(this, KmLitroActivity::class.java)
+            intent.putExtra("DISTANCIA",edtDistanciaValor)
+            startActivity(intent)
         }
     }
 }
